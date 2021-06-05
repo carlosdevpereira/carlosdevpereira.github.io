b59408f4f8ad9d31f7c94f8f78dac7a03d7c427c
Carlos Pereira <carlos.pereira@kentratech.com>
2021-06-05 20:34:45 +0100

    v1.0.0

---
+++ precache-manifest.44e6ed9dae7f775865f68f09305abb5b.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee73a70ab3d62895c704",
+    "url": "/css/app.cd2ac791.css"
+  },
+  {
+    "revision": "66257a9a8dd6b2c81a3798628df2e6cf",
+    "url": "/fonts/Exo/4UaOrEtFpBISc36j2jDu5w.woff2"
+  },
+  {
+    "revision": "2d000a48b8d4cfb00b91ecb4e6841846",
+    "url": "/fonts/Exo/4UaOrEtFpBISfH6j2jDu55XI.woff2"
+  },
+  {
+    "revision": "bf986b37f7a4f7a8adf455cf21a379d2",
+    "url": "/fonts/Exo/4UaOrEtFpBISfX6j2jDu55XI.woff2"
+  },
+  {
+    "revision": "e7b74646b58bea7f71a0d5ddb954220b",
+    "url": "/fonts/Exo/Exo.css"
+  },
+  {
+    "revision": "988875e4624e160b85b2a4d2b91cc7a1",
+    "url": "/fonts/Fontello/Fontello.css"
+  },
+  {
+    "revision": "9d772d6ed32e0f99705341c760b43b77",
+    "url": "/fonts/Fontello/fontello.eot"
+  },
+  {
+    "revision": "5320114123374b8e6ac140b5820c9c81",
+    "url": "/fonts/Fontello/fontello.svg"
+  },
+  {
+    "revision": "8dd1b01d7779e58e82843f09bb867923",
+    "url": "/fonts/Fontello/fontello.ttf"
+  },
+  {
+    "revision": "cac46d3bc623f52145a07ab62bf3b2c3",
+    "url": "/fonts/Fontello/fontello.woff"
+  },
+  {
+    "revision": "212778a3ff6c74859e6c37b1473bc615",
+    "url": "/fonts/Fontello/fontello.woff2"
+  },
+  {
+    "revision": "a20a0c1ce575e32678b313b8551e58ec",
+    "url": "/img/articles/git.webp"
+  },
+  {
+    "revision": "c2260527b16f180a8443da5bdf1a2164",
+    "url": "/img/contacts.webp"
+  },
+  {
+    "revision": "943f3a67b2e9f143e316453b7b3ac151",
+    "url": "/img/flags/en.webp"
+  },
+  {
+    "revision": "54857ff5d11d2b8bb5e0f32ddac27d1f",
+    "url": "/img/flags/pt.webp"
+  },
+  {
+    "revision": "ddce972e14ba21f5a4512ee729ec0dc8",
+    "url": "/img/logo.webp"
+  },
+  {
+    "revision": "ba7ef24cf6f810daca72914c897984c8",
+    "url": "/img/photo.webp"
+  },
+  {
+    "revision": "63778cf8b6b0382c49b87e1deef7a120",
+    "url": "/img/photo@1x.webp"
+  },
+  {
+    "revision": "2175e89584a87c5ea530d342778cfdcb",
+    "url": "/img/technologies.webp"
+  },
+  {
+    "revision": "2b54fa832561026680652d70da84a27d",
+    "url": "/img/testimonials/ibermeteo-logo.webp"
+  },
+  {
+    "revision": "4f025ae734ba3b402b7f9da1696c3439",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee73a70ab3d62895c704",
+    "url": "/js/app.0f29a877.js"
+  },
+  {
+    "revision": "ac8f16bb1445796db3e0",
+    "url": "/js/chunk-vendors.300c5fd6.js"
+  },
+  {
+    "revision": "8242973af7e235a5d44bfd082626b0e7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "defe1fc432c48b106aa561af23f97118",
+    "url": "/svg11.dtd"
+  }
+]);
